4f460a2d127d71d04a6a31a4f9df4a1e79801302
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-03-21 20:36:32 +0100

    Mise en place de la gestion d'évènements (detection souris + clavier)

--- main.c
+++ main.c
@@ -1,143 +1,105 @@
-#include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdio.h>
-int main(int argc, char *argv[])
+#include <SDL/SDL.h>
+#include <SDL_image.h>
+
+/*Appels des fonctions*/
+//void pause();
+
+/*test*/
+int test(int argc, char *argv[])
 {
 
-    /* Initialisation simple */
-    if(SDL_Init(SDL_INIT_VIDEO) != 0 ){
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        return -1;
-    }
-
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_CENTERED,
-                                                                  SDL_WINDOWPOS_CENTERED,
-                                                                  640,
-                                                                  480,
-                                                                  SDL_WINDOW_SHOWN);
-        //CreateWindow : Nom à afficher / pos en X / pos en Y /Largeur / Hauteur / options
-        SDL_Renderer *pRenderer = SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED); // Création d'un SDL_Renderer utilisant l'accélération matérielle
-
-        /*Image de Background*/
-        if(pRenderer)
-        {
-            SDL_Surface  *pSprite = SDL_LoadBMP("qui.bmp");
-            if(pSprite)
-            {
-                SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer,pSprite); // Préparation du sprite
-                if(pTexture)
-                {
-                    SDL_Rect dest = { 640/2 - pSprite->w/2,480/2 - pSprite->h/2, pSprite->w, pSprite->h};
-                    SDL_RenderCopy(pRenderer,pTexture,NULL,&dest); // Copie du sprite grâce au SDL_Renderer
-
-                    SDL_RenderPresent(pRenderer); // Affichage
-                    //SDL_Delay(1000); // Attendre trois secondes, que l'utilisateur voit la fenêtre
-
-                    SDL_DestroyTexture(pTexture); // Libération de la mémoire associée à la texture
-                }
-                else
-                {
-                    fprintf(stdout,"Échec de création de la texture (%s)\n",SDL_GetError());
-                }
-
-                SDL_FreeSurface(pSprite); // Libération de la ressource occupée par le sprite
-            }
-            else
-            {
-                fprintf(stdout,"Échec de chargement du sprite (%s)\n",SDL_GetError());
-            }
-
-            SDL_DestroyRenderer(pRenderer); // Libération de la mémoire du SDL_Renderer
-        }
-        else
-        {
-            fprintf(stdout,"Échec de création du renderer (%s)\n",SDL_GetError());
-        }
-
-        /*Gestion de la fenêtre*/
-        if( pWindow )
-        {
-            //SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-            char cont = 1; // Détermine si on continue la boucle principale
-            SDL_Event event;
-
-            while ( cont != 0 )
-            {
-                while ( SDL_PollEvent(&event) )
-                {
-                    // Traitement de l'événement
-                    switch (event.type) /* Quel événement avons-nous ? */
-                    {
-                        case SDL_KEYDOWN:
-                            fprintf(stdout, "Un appuie sur une touche :\n");
-                            fprintf(stdout, "\trépétition ? : %d\n",event.key.repeat);
-                            fprintf(stdout, "\tscancode : %d\n",event.key.keysym.scancode);
-                            fprintf(stdout, "\tkey : %d\n",event.key.keysym.sym);
-                            if ( event.key.keysym.scancode == SDL_SCANCODE_ESCAPE )
-                            {
-                                cont = 0;
-                            }
-                            break;
-                        case SDL_KEYUP:
-                            fprintf(stdout, "Un relachement d'une touche :\n");
-                            fprintf(stdout, "\trépétition ? : %d\n",event.key.repeat);
-                            fprintf(stdout, "\tscancode : %d\n",event.key.keysym.scancode);
-                            fprintf(stdout, "\tkey : %d\n",event.key.keysym.sym);
-                            break;
-
-                        case SDL_MOUSEMOTION:
-                            fprintf(stdout, "Un déplacement de la souris :\n");
-                            fprintf(stdout, "\tfenêtre : %d\n",event.motion.windowID);
-                            fprintf(stdout, "\tsouris : %d\n",event.motion.which);
-                            fprintf(stdout, "\tposition : %d;%d\n",event.motion.x,event.motion.y);
-                            fprintf(stdout, "\tdelta : %d;%d\n",event.motion.xrel,event.motion.yrel);
-                            break;
-                        case SDL_MOUSEBUTTONUP:
-                            fprintf(stdout, "Un relachement d'un bouton de la souris :\n");
-                            fprintf(stdout, "\tfenêtre : %d\n",event.button.windowID);
-                            fprintf(stdout, "\tsouris : %d\n",event.button.which);
-                            fprintf(stdout, "\tbouton : %d\n",event.button.button);
-    #if SDL_VERSION_ATLEAST(2,0,2)
-                            fprintf(stdout, "\tclics : %d\n",event.button.clicks);
-    #endif
-                            fprintf(stdout, "\tposition : %d;%d\n",event.button.x,event.button.y);
-                            break;
-                        case SDL_MOUSEBUTTONDOWN:
-                            fprintf(stdout, "Un appuie sur un bouton de la souris :\n");
-                            fprintf(stdout, "\tfenêtre : %d\n",event.button.windowID);
-                            fprintf(stdout, "\tsouris : %d\n",event.button.which);
-                            fprintf(stdout, "\tbouton : %d\n",event.button.button);
-    #if SDL_VERSION_ATLEAST(2,0,2)
-                            fprintf(stdout, "\tclics : %d\n",event.button.clicks);
-    #endif
-                            fprintf(stdout, "\tposition : %d;%d\n",event.button.x,event.button.y);
-                            break;
-
-                        case SDL_WINDOWEVENT:
-                            fprintf(stdout, "Un événement de fenêtre, sur la fenêtre : %d\n",event.window.windowID);
-                            // En théorie, ici, il faudrait faire un autre test ou switch pour chaque type de cet événement
-                            break;
-                        default:
-                            fprintf(stdout, "Événement non traité : %d\n",event.type);
-                    }
-
-                    fprintf(stdout, "\n");
-                }
-
-                // On a traité les événements, on peut continuer le jeu
-            }
-
-
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
-    }
-
-    SDL_Quit();
 }
+/*Programme principal*/
+int main(int argc, char *argv[])
+{
+    /*Allocation de la mémoire pour chaque élèments SDL*/
+        SDL_Surface *fenetre = NULL, *imagebg=NULL, *rectangle = NULL;
+
+    /*Declaration de la variable d'évènements*/
+        SDL_Event evenement;
+        int boucle = 1;
+
+    /*Initialisation de la position de chaque element*/
+        SDL_Rect positionRect, positionFond;
+
+    /*Préparation de la bibliothèque SDL avec son module audio et vidéo seulement*/
+        SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER);
+
+    /*Titre du logiciel*/
+        SDL_WM_SetCaption("Titre du projet", NULL);
+
+    /*Icone du logiciel (doit se trouver avant la création de la fenêtre)*/
+        SDL_WM_SetIcon(IMG_Load("qui.bmp"), NULL);
+
+    //Préparation de l'élèment Background qui sera affiché
+        fenetre = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
+            if (fenetre == NULL) // Si l'ouverture a échoué, on le note et on arrête
+            {
+                fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
+                exit(EXIT_FAILURE);
+            }
+    //Initialisation de l'emplacement de chaque élèment avant la boucle
+        positionFond.x=0;
+        positionFond.y=0;
+        positionRect.x = 0;
+        positionRect.y = 0;
+
+    /*"Attente" des actions de l'utilisateur*/
+        while(boucle){
+            SDL_WaitEvent(&evenement); // Récupération de l'évènement
+            switch(evenement.type){//Type d'évènement
+                case SDL_QUIT: // Arrêt du programme
+                    boucle = 0;
+                    break;
+                case SDL_KEYDOWN: //Appui sur une touche
+                    switch(evenement.key.keysym.sym){// Analyse de la touche
+                        case SDLK_ESCAPE://Echap
+                            boucle=0;
+                            break;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONUP: // Clic souris
+                    switch(evenement.button.button){ //Analyse de l'équipement de la souris
+                        case SDL_BUTTON_RIGHT:
+                            positionRect.x=evenement.button.x;
+                            positionRect.y=evenement.button.y;
+                            break;
+                    }
+                    break;
+                case SDL_MOUSEMOTION:
+                    positionRect.x = evenement.motion.x;
+                    break;
+                case SDL_VIDEORESIZE:
+                    positionFond.x=evenement.resize.w/2 - imagebg->w / 2;
+                    positionFond.y=evenement.resize.h/2 - imagebg->h / 2;
+                    break;
+            }
+
+            /*Affichage des élèments + background à chaque tour de boucle*/
+                //Coloration du fond
+                    SDL_FillRect(fenetre, NULL, SDL_MapRGB(fenetre->format, 255, 255, 112));
+                //Application d'une image en guise de background
+                    imagebg = IMG_Load("qui.bmp");
+                        //Si un fond BLEU est présent sur une image que l'on veut rendre transparente:
+                    SDL_SetColorKey(imagebg, SDL_SRCCOLORKEY, SDL_MapRGB(imagebg->format, 0, 0, 255));
+                    SDL_BlitSurface(imagebg, NULL, fenetre, &positionFond);
+
+                //Caractéristiques de l'élèment "rectangle"
+                    rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE, 220, 180, 32, 0, 0, 0, 0);
+
+                    SDL_FillRect(rectangle, NULL, SDL_MapRGB(fenetre->format, 17, 206, 112));
+                    SDL_BlitSurface(rectangle, NULL, fenetre, &positionRect);//Application de l'élèment sur le background
+
+                /*Mise à jour de la fenêtre avec les élèments modifiés*/
+                    SDL_Flip(fenetre);
+        }
+
+    /*Libération de la mémoire qui était allouée à SDL*/
+        SDL_FreeSurface(imagebg);
+        SDL_FreeSurface(rectangle);
+        SDL_QUIT;
+    return EXIT_SUCCESS; //Similaire à un renvoi OK pour tous les OS
+}
+
