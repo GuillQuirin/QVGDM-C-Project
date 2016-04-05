a4b5a75c863d9a1cdae8633951e76996fbe1648b
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-04-05 09:12:25 +0200

    Scission entre le main() et la fonction Menu()

--- main.cpp
+++ main.cpp
@@ -11,18 +11,13 @@
 
 int interieurClic(SDL_Event event, SDL_Rect zone, int longueur, int hauteur);
 int interieurMove(SDL_Event event, SDL_Rect zone, int longueur, int hauteur);
+int menu(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond);
 
 /*Programme principal -> Déclaration des pointeurs*/
 int main(int argc, char *argv[])
 {
-    int cpt=0;
-
     /*Allocation de la mémoire pour chaque élèments SDL*/
         SDL_Surface *fenetre = NULL, *imagebg=NULL;
-        SDL_Surface *texte1=NULL, *texte2=NULL, *texte3=NULL, *texte4=NULL;
-        SDL_Surface *rectangle1=NULL, *rectangle2=NULL, *rectangle3=NULL, *rectangle4=NULL;
-
-        int Reclongueur=250, Rechauteur=60;
 
     /* Initialise SDL_Mixer */
         int flags=MIX_INIT_MP3;
@@ -43,29 +38,19 @@ int main(int argc, char *argv[])
             Mix_FreeMusic(musique);
          }
 
-         /* On charge la nouvelle chanson */
+         // On charge la nouvelle chanson
          musique = Mix_LoadMUS( "mu.mp3" );
 
         /* On active la répétition de la musique à l'infini */
         Mix_PlayMusic(musique, -1);
 
-    /*Declaration de la variable d'évènements*/
-        SDL_Event evenement;
-        int boucle = 1;
-
-    /*Déclaration des styles pour le texte*/
-        TTF_Font *police=NULL;
-        SDL_Color couleurNoire = {0,0,0};
-
-    /*Gestion du chronomètre dans le jeu*/
+    /*Gestion du chronomètre dans le jeu
         char temps[20];
         float seconde=60.0;
-        float temps0=0.0;
+        float temps0=0.0;*/
 
     /*Initialisation de la position de chaque element*/
         SDL_Rect positionFond;
-        SDL_Rect positionRect1,positionRect2,positionRect3,positionRect4;
-        SDL_Rect positionTexte1, positionTexte2, positionTexte3, positionTexte4;
 
     /*Préparation de la bibliothèque SDL avec son module audio et vidéo + texte*/
         SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER);
@@ -81,17 +66,64 @@ int main(int argc, char *argv[])
     //Préparation de l'élèment Background qui sera affiché
         fenetre = SDL_SetVideoMode(SCREEN_WIDTH , SCREEN_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF );//| SDL_RESIZABLE);
 
-    //Chargement de la police
+
+
+    //Initialisation de l'emplacement de chaque élèment avant la boucle
+        positionFond.x=0;
+        positionFond.y=0;
+
+    //Application d'une image en guise de background
+    imagebg = IMG_Load("qui.jpg");
+        //Si un fond BLEU est présent sur une image que l'on veut rendre transparente:
+    SDL_SetColorKey(imagebg, SDL_SRCCOLORKEY, SDL_MapRGB(imagebg->format, 0, 0, 255));
+
+    int retour =1;
+
+    /*"Attente" des actions de l'utilisateur*/
+        do{
+            switch(retour){
+                case 1:
+                        retour = menu(fenetre,imagebg, positionFond);
+                        break;
+            }
+        }
+        while(retour!=0);
+
+    /*Libération de la mémoire qui était allouée à SDL*/
+        Mix_FreeMusic(musique);
+        Mix_CloseAudio();
+        Mix_Quit();
+        TTF_Quit();
+        SDL_FreeSurface(imagebg);
+        SDL_QUIT;
+    return EXIT_SUCCESS; //Similaire à un renvoi OK pour tous les OS
+}
+
+int menu(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
+
+        SDL_Surface *texte1=NULL, *texte2=NULL, *texte3=NULL, *texte4=NULL;
+        SDL_Surface *rectangle1=NULL, *rectangle2=NULL, *rectangle3=NULL, *rectangle4=NULL;
+
+        SDL_Rect positionRect1,positionRect2,positionRect3,positionRect4;
+        SDL_Rect positionTexte1, positionTexte2, positionTexte3, positionTexte4;
+
+        SDL_Color couleurNoire = {0,0,0};
+
+        /*Déclaration des styles pour le texte*/
+        TTF_Font *police=NULL;
+
+        /*Declaration de la variable d'évènements*/
+        SDL_Event evenement;
+        int boucle = 1, renvoi=1;
+        int Reclongueur=250, Rechauteur=60;
+
+        //Chargement de la police
         police=TTF_OpenFont("police.ttf",25);
         TTF_SetFontStyle(police,TTF_STYLE_NORMAL);//, TTF_STYLE_ITALIC | TTF_STYLE_UNDERLINE);
         texte1 = TTF_RenderText_Blended(police, "JOUER", couleurNoire);
         texte2 = TTF_RenderText_Blended(police, "SAUVEGARDE", couleurNoire);
         texte3 = TTF_RenderText_Blended(police, "OPTIONS", couleurNoire);
         texte4 = TTF_RenderText_Blended(police, "QUITTER", couleurNoire);
-
-    //Initialisation de l'emplacement de chaque élèment avant la boucle
-        positionFond.x=0;
-        positionFond.y=0;
 
         positionRect1.x = 40;
         positionRect1.y = 300;
@@ -117,22 +149,13 @@ int main(int argc, char *argv[])
         positionTexte4.x=positionRect4.x+(Reclongueur/4);
         positionTexte4.y=positionRect4.y+(Rechauteur/4);
 
-float test=0.0;
-test = timer(test);
+        //Caractéristiques des élèments "rectangles"
+        rectangle1 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
+        rectangle2 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
+        rectangle3 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
+        rectangle4 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
 
-    //Application d'une image en guise de background
-    imagebg = IMG_Load("qui.jpg");
-        //Si un fond BLEU est présent sur une image que l'on veut rendre transparente:
-    SDL_SetColorKey(imagebg, SDL_SRCCOLORKEY, SDL_MapRGB(imagebg->format, 0, 0, 255));
-
- //Caractéristiques des élèments "rectangles"
-    rectangle1 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
-    rectangle2 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
-    rectangle3 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
-    rectangle4 = SDL_CreateRGBSurface(SDL_HWSURFACE, Reclongueur, Rechauteur, 32, 0, 0, 0, 0);
-
-    /*"Attente" des actions de l'utilisateur*/
-        while(boucle){
+         while(boucle){
 
             /*if(seconde !=-1.0){
                 sprintf(temps, "%.1f", seconde);
@@ -154,7 +177,7 @@ test = timer(test);
                     switch(evenement.button.button){ //Analyse de l'équipement de la souris
                         case SDL_BUTTON_LEFT:
                             if(interieurClic(evenement,positionRect4, Reclongueur, Rechauteur)){
-                                boucle=0;
+                                boucle=0;renvoi=0;
                             }
                             break;
                     }
@@ -177,13 +200,7 @@ test = timer(test);
                         SDL_FillRect(rectangle4, NULL, SDL_MapRGB(fenetre->format, 255, 206, 112));
                        }
                     break;
-                case SDL_VIDEORESIZE:
-                    positionFond.x=evenement.resize.w/2 - imagebg->w / 2;
-                    positionFond.y=evenement.resize.h/2 - imagebg->h / 2;
-                    break;
             }
-
-
 
             /*Affichage des élèments + background à chaque tour de boucle*/
                 //Coloration du fond
@@ -211,24 +228,17 @@ test = timer(test);
                 /*Mise à jour de la fenêtre avec les élèments modifiés*/
                     SDL_Flip(fenetre);
         }
-
-    /*Libération de la mémoire qui était allouée à SDL*/
-        Mix_FreeMusic(musique);
-        Mix_CloseAudio();
-        Mix_Quit();
-        TTF_CloseFont(police);
-        TTF_Quit();
-        SDL_FreeSurface(texte1);
-        SDL_FreeSurface(texte2);
-        SDL_FreeSurface(texte3);
-        SDL_FreeSurface(texte4);
-        SDL_FreeSurface(imagebg);
         SDL_FreeSurface(rectangle1);
         SDL_FreeSurface(rectangle2);
         SDL_FreeSurface(rectangle3);
         SDL_FreeSurface(rectangle4);
-        SDL_QUIT;
-    return EXIT_SUCCESS; //Similaire à un renvoi OK pour tous les OS
+        SDL_FreeSurface(texte1);
+        SDL_FreeSurface(texte2);
+        SDL_FreeSurface(texte3);
+        SDL_FreeSurface(texte4);
+        TTF_CloseFont(police);
+
+        return renvoi;
 }
 
 int interieurClic(SDL_Event event, SDL_Rect zone, int longueur, int hauteur)
