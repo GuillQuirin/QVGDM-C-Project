ef47d2d8e657c98c3c0fcdcecc5a08618a367cbf
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-04-06 09:05:35 +0200

    Mise en place du mapping et de la réorientation pour chaque fenêtre du logiciel

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@
 int interieurClic(SDL_Event event, SDL_Rect zone, int longueur, int hauteur);
 int interieurMove(SDL_Event event, SDL_Rect zone, int longueur, int hauteur);
 int menu(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond);
+int difficulte(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond);
+int partie(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond, int type);
+int sauvegarde(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond);
+int option(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond);
 
 /*Programme principal -> Déclaration des pointeurs*/
 int main(int argc, char *argv[])
@@ -19,56 +23,44 @@ int main(int argc, char *argv[])
     /*Allocation de la mémoire pour chaque élèments SDL*/
         SDL_Surface *fenetre = NULL, *imagebg=NULL;
 
-    /* Initialise SDL_Mixer */
+    /* Initialisiation de la musique */
         int flags=MIX_INIT_MP3;
         int initted=Mix_Init(flags);
-
-        /* open 44.1KHz, signed 16bit, system byte order,stereo audio, using 1024 byte chunks */
+        //open 44.1KHz, signed 16bit, system byte order,stereo audio, using 1024 byte chunks
         Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
-
-        /* Définit le nombre de channels à mixer */
+        //Définit le nombre de channels à mixer
         Mix_AllocateChannels(32);
-
         Mix_Music *musique=NULL;
-
-        /* On libère la chanson précédente s'il y en a une */
+        //On libère la chanson précédente s'il y en a une */
          if ( musique != NULL )
          {
             Mix_HaltMusic();
             Mix_FreeMusic(musique);
          }
-
          // On charge la nouvelle chanson
          musique = Mix_LoadMUS( "mu.mp3" );
-
-        /* On active la répétition de la musique à l'infini */
+        //On active la répétition de la musique à l'infini
         Mix_PlayMusic(musique, -1);
 
-    /*Gestion du chronomètre dans le jeu
-        char temps[20];
+    /*Gestion du chronomètre dans le jeu*/
+        /*char temps[20];
         float seconde=60.0;
         float temps0=0.0;*/
-
-    /*Initialisation de la position de chaque element*/
-        SDL_Rect positionFond;
 
     /*Préparation de la bibliothèque SDL avec son module audio et vidéo + texte*/
         SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER);
         SDL_putenv("SDL_VIDEO_CENTERED=center");
         TTF_Init();
 
-    /*Titre du logiciel*/
-        SDL_WM_SetCaption("Titre du projet", NULL);
-
-    /*Icone du logiciel (doit se trouver avant la création de la fenêtre)*/
+    /*Titre et icone du logiciel (doit se trouver avant la création de la fenêtre)*/
+        SDL_WM_SetCaption("Qui veut gagner du pognon", NULL);
         SDL_WM_SetIcon(IMG_Load("qui.bmp"), NULL);
 
     //Préparation de l'élèment Background qui sera affiché
         fenetre = SDL_SetVideoMode(SCREEN_WIDTH , SCREEN_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF );//| SDL_RESIZABLE);
 
-
-
-    //Initialisation de l'emplacement de chaque élèment avant la boucle
+    //Initialisation de l'emplacement du fond
+        SDL_Rect positionFond;
         positionFond.x=0;
         positionFond.y=0;
 
@@ -85,6 +77,26 @@ int main(int argc, char *argv[])
                 case 1:
                         retour = menu(fenetre,imagebg, positionFond);
                         break;
+                case 2:
+                        retour = difficulte(fenetre,imagebg, positionFond);
+                        break;
+                /*3 difficultés */
+                case 21:
+                case 22:
+                case 23:
+                /*3 sauvegardes*/
+                case 31:
+                case 32:
+                case 33:
+                        retour = partie(fenetre,imagebg, positionFond, retour);
+                        break;
+                case 4:
+                        retour = sauvegarde(fenetre,imagebg, positionFond);
+                        break;
+                case 5:
+                        retour = option(fenetre,imagebg, positionFond);
+                        break;
+
             }
         }
         while(retour!=0);
@@ -98,6 +110,8 @@ int main(int argc, char *argv[])
         SDL_QUIT;
     return EXIT_SUCCESS; //Similaire à un renvoi OK pour tous les OS
 }
+
+/*Liste des fenêtres*/
 
 int menu(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
 
@@ -240,6 +254,21 @@ int menu(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
 
         return renvoi;
 }
+
+int difficulte(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
+    return 1;
+}
+int partie(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond, int type){
+    return 1;
+}
+int sauvegarde(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
+    return 1;
+}
+int option(SDL_Surface *fenetre, SDL_Surface *imagebg, SDL_Rect positionFond){
+    return 1;
+}
+
+/*Fonctions utiles*/
 
 int interieurClic(SDL_Event event, SDL_Rect zone, int longueur, int hauteur)
 {
